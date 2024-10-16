58070e7a4ea1d6bb87d4579a167a0c8d211a5fec
Anish Shinde <125127441+anishshinde1214@users.noreply.github.com>
2024-10-16 18:26:34 +0530

    Multiple questions

---
+++ Q9.py
@@ -0,0 +1,60 @@
+# Q9 check number is odd or even
+
+A = 25
+if A % 2 == 0:
+    print(A, 'is Even number')
+else:
+    print(A, 'is odd')
+
+#############################################################33
+
+# Q10 check year is leap or not
+
+year = 2024
+if (year % 400) == 0:
+    print (year,'is leap year')
+elif (year % 4) ==0 and (year % 100) != 0:
+    print(year,'is leap year')
+else:
+    print('not a leap year')
+
+###############################################
+
+# Q11
+
+A = 4
+if A == 1:
+    print('yes python certified')
+elif A == 2:
+    print('yes java certified')
+else:
+    print('invalid user')
+
+################################################################
+
+ #12
+
+BS = 2000
+TA = BS*10/100
+DA = BS*20/100
+HRA = BS*15/100
+
+GS = TA+DA+HRA+BS
+
+print(GS)
+
+################################################################
+
+# Q13 check triangle is valid or not
+A = 40
+B = 50
+C = 10
+
+if  A+B+C == 180:
+    print('triangle is valid')
+else:
+    print('triangle is not valid')
+
+#############################################################################3
+
+
